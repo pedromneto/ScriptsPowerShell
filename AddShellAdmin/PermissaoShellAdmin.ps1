d2465b38c996f50bc6072036022198d2d66f2daf
Pedro Marques M. Neto <pedro.neto@outlook.com>
2016-05-23 16:23:43 -0300

    Script Permissão de ShellAdmin
    Concede permissão de ShellAdmin nas bases de conteúdo da WebApplication

---
+++ AddShellAdmin/PermissaoShellAdmin.ps1
@@ -0,0 +1,36 @@
+﻿Add-PSSnapin Microsoft.SharePoint.PowerShell
+cls
+#Parametros
+
+$urlSite = "http://seusite"
+
+$usuarios = @("dominio\usuario1","dominio\usuario2")
+
+#Fim dos parâmetros
+
+
+$site = Get-SPSite $urlSite
+
+$usuarios.ForEach({
+    Write-Host "Usuario Atual: $_" -ForegroundColor Yellow
+    $usuarioAtual = $_
+    $ContentDataBases = $site.WebApplication.ContentDatabases
+    $ContentDataBases.ForEach({
+        Write-Host "   Concedendo permissao na base de conteudo" $_.Name
+        $ContentDataBaseID = $_.Id    
+        
+        try
+        {
+            Add-SPShellAdmin -UserName $usuarioAtual  -database $ContentDataBaseID
+            Write-Host "   Ok" -ForegroundColor Green
+        }
+        catch
+        {
+            Write-Host "   Erro:" $_.Exception.Message -ForegroundColor Red
+        }
+    })
+    Write-Host ""
+    
+})
+
+Write-Host "Fim"
